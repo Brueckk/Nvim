753ba4b8258757174baf64512c68a0c860d35aea
Brueckk <thomasbrueckgit@gmail.com>
2024-01-08 13:48:29 -0500

    script for installation

---
+++ nvimInstall/scriptInstall.sh
@@ -0,0 +1,13 @@
+" STEPS FOR INSTALLING UPDATED NEOVIM AND LAZYVIM "
+
+sudo apt-get install fuse
+
+hmod u+x nvim.appimage && ./nvim.appimage
+
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+sudo apt install fd-find
+
+sudo apt install ripgrep
+
+sudo apt install g++
