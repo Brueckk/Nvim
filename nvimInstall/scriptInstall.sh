9d3378a9ab0cf06578036f461fd3cdd4f5ebe0c3
Brueckk <thomasbrueckgit@gmail.com>
2024-01-08 13:55:43 -0500

    updated script to execute

--- nvimInstall/scriptInstall.sh
+++ nvimInstall/scriptInstall.sh
@@ -6,8 +6,14 @@ hmod u+x nvim.appimage && ./nvim.appimage
 
 sudo mv nvim.appimage /usr/local/bin/nvim
 
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
+
 sudo apt install fd-find
 
 sudo apt install ripgrep
 
 sudo apt install g++
+
+nvim
